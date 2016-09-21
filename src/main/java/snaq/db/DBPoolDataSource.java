39278d03f323a689e96cfc482279ae8177ad4af5
gwinstanley <support@snaq.net>
2016-09-21 16:30:55 +1000

    Bug fix for DBPoolDataSource.setMinPool method.

--- src/main/java/snaq/db/DBPoolDataSource.java
+++ src/main/java/snaq/db/DBPoolDataSource.java
@@ -486,7 +486,7 @@ public class DBPoolDataSource implements DataSource, ConnectionPoolListener
    */
   public synchronized void setMinPool(int minPool)
   {
-    this.maxPool = minPool;
+    this.minPool = minPool;
     if (pool != null)
       pool.setParameters(this.minPool, pool.getMaxPool(), pool.getMaxSize(), pool.getIdleTimeout());
   }
